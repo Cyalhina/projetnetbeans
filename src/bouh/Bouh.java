0af109cd20f3e81808b0d121cd11b8573a064b8e
Yaelle <ya.asso@laposte.net>
2017-11-23 16:36:24 +0100

    Premier commit

--- src/bouh/Bouh.java
+++ src/bouh/Bouh.java
@@ -17,5 +17,13 @@ public class Bouh {
     public static void main(String[] args) {
         System.out.println("coucou");
     }
-    
+        public static int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
